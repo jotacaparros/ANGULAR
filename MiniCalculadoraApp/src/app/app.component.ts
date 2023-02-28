7400b8b915d944ac3f838a6ef13ca99b7a38f8af
jotacaparros <jotacaparros@gmail.com>
2023-02-28 14:44:16 +0100

    Calculadora sencilla

--- MiniCalculadoraApp/src/app/app.component.ts
+++ MiniCalculadoraApp/src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mini Calculadora';
+  registro = 'Registro de usuarios';
+  mensaje = '';
+  registrado=false;
   numero1:number=0;
   numero2:number=0;
   resultado:number=0;
@@ -18,5 +21,10 @@ export class AppComponent {
 
   restar():void{
     this.resultado=this.numero1 - this.numero2;
+  }
+
+  registrarUsuario(){
+    this.registrado=true;
+    this.mensaje="Registro completado con éxito";
   }
 }
